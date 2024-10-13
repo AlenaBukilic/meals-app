eaa10d38490ead63e2f595bdd6389fa4d319e423
Alena Bukilic <bukilicalena@gmail.com>
2024-10-13 20:57:35 +0100

    add react navigation container and stack screen melascategories

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// export default function App() {
-//   return (
-//     <>
-//       <StatusBar style="light" />
-//       <CategoriesScreen />;
-//     </>
-//   );
-// }
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
